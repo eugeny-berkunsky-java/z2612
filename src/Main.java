f1c9618d77d471e57322c8988f2798d1f0fd0db4
kafedra-iust <kafedra.iust@gmail.com>
2020-12-26 17:09:34 +0200

    z2

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    int[] a = new int[4];
-        Scanner in = new Scanner(System.in);
+
+        int[][] q = new int[3][];
+        q[0] = new int[3];
+        q[1] = new int[3];
+        q[2] = new int[3];
+
+        int[][] m = { {2, 1, 5}, {6, 7, 3}, {0, 8, 0} };
+        double x = 2.5;
+        double y = Math.sin(x) * 2 + 7;
+
+        boolean f = check(m);
+        System.out.println(f);
+    }
+
+    static boolean check(int[][] a) {
+        // По очереди проверяем строки + столбцы
+        // если сумма элементов строки != сумме эл-тов столбца => false
         for (int i = 0; i < a.length; i++) {
-            a[i] = in.nextInt();
-        }
-        int max = a[0];
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] > max) max = a[i];
-        }
-        for (int k = max; k > 0; k--) {
-            for (int i = 0; i < a.length; i++) {
-                if (a[i] >= k) System.out.print("* ");
-                else System.out.print("  ");
+            int sr = 0;
+            for (int j = 0; j < a.length; j++) {
+                sr += a[i][j];
             }
-            System.out.println();
+            int sc = 0;
+            for (int j = 0; j < a.length; j++) {
+                sc += a[j][i];
+            }
+            if (sr != sc) return false;
         }
+        // если дошли до конца => true
+        return true;
     }
 }
